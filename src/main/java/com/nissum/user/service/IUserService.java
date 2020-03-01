e3773a8d0e98df34684b1b11837f3dd93aa7b67f
fabkius <fabian.urzua.b@gmail.com>
2020-03-01 13:52:32 -0300

    Update IUserService.java

--- src/main/java/com/nissum/user/service/IUserService.java
+++ src/main/java/com/nissum/user/service/IUserService.java
@@ -4,6 +4,5 @@ import com.nissum.user.domain.UserDto;
 import com.nissum.user.domain.UserRsDto;
 
 public interface IUserService {
-    UserDto getUser(Long id);
     UserRsDto save(UserDto user);
 }
